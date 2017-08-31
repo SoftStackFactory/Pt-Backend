2844c43df2c20403a53a0647c9af4ad57c621e26
Peter Horton <marenkb@gmail.com>
2017-08-31 04:58:51 +0000

    first attempt at organization request email

--- common/models/organization.js
+++ common/models/organization.js
@@ -1,5 +1,35 @@
 'use strict';
 
+var mailgun = require('../../mailgun/orgRequest.js');
+
+
 module.exports = function(Organization) {
+    
+    Organization.request = function(userId, name, website, cb) {
+        
+        mailgun.sendEmail(userId, name, website);
+        
+        console.log("New Organization request email sent");
+        
+        
+    };
+    
+    Organization.remoteMethod(
+    'request', {
+      http: {
+        path: '/request',
+        accepts: [{arg: 'userId', type: 'string' }, 
+            { arg: 'name', type: 'string' },
+            { arg: 'website', type: 'string' }],
+        verb: 'post'
+      },
+      returns: {
+        
+        arg: 'status',
+        type: 'string'
+      }
+    }
+    
+  );
 
 };
