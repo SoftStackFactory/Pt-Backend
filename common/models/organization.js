258556b59c727d5d10264ddcdc0b404b3eec8e11
Peter Horton <marenkb@gmail.com>
2017-09-03 16:47:10 +0000

    added website to email requesting to become organization

--- common/models/organization.js
+++ common/models/organization.js
@@ -5,35 +5,30 @@ var mailgun = require('../../mailgun/orgRequest.js');
 
 module.exports = function(Organization) {
     
-    //Organization.request = function(userId, name, website, cb) {
-        //mailgun.sendEmail(userId, name, website);
-    Organization.request = function (cb) {    
-        
-        var response;
-        
-        mailgun.sendEmail();
+    Organization.request = function( website, cb) {
+      mailgun.sendEmail(website);
+    //Organization.request = function (cb) {    
+        //mailgun.sendEmail();
         
         console.log("New Organization request email sent");
-        
-        response: 'New Organization request email sent';
-        
+        var response = 'New Organization request email sent for ' + website;
         cb(null, response);
+        console.log('response', response);
         
     };
     
     Organization.remoteMethod(
     'request', {
-      http: {
-        path: '/request',
-        /*
+      http: { path: '/request', verb: 'post' },
+      
+      /*
         accepts: [{arg: 'userId', type: 'string' }, 
             { arg: 'name', type: 'string' },
             { arg: 'website', type: 'string' }],
         */
-        verb: 'post'
-      },
+      
+      accepts: { arg: 'website', type: 'string'},
       returns: {
-        
         arg: 'request',
         type: 'string'
       }
