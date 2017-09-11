c113d0f243895f3e941e58d604528d2f08efb211
vhoangssf <vincenthoang00@gmail.com>
2017-09-11 21:44:36 +0000

    Added strongloop unique categories and changed second set of mock questions to have different categories

--- common/models/question.js
+++ common/models/question.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function(Question) {
-
-};
+Question.uniqueCategories= function ( cb) {
+    var productCollection = Question.getDataSource().connector.collection(Question.modelName),
+        uniqueTags = productCollection.distinct('category', function (err, results) {
+            cb(null, results)
+        })
+}
+    Question.remoteMethod(
+    'uniqueCategories', {
+      http: {
+        path: '/uniqueCategories',
+        verb: 'get',
+        },
+         description: 'Get  list of unique b Questions category',
+        returns: { arg: 'data', type: ['Question'], root: true }
+    });
+}
